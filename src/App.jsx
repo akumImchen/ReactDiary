efdc4dee98e2c350db81e5cfce33e142f21b3eaf
akumImchen <akumimchen1411@gmail.com>
2025-09-18 00:27:25 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Main from "./Components/Main";
+import Navbar from "./Components/Navbar";
+import "./index.css"
+
+export default function App() {
+    return (
+        <>
+             <Navbar />
+            <Main />
+           
+        </>
+
+    )
+}
