bc8c6166ae003bb66398090e593389ddb6341da2
akumImchen <akumimchen1411@gmail.com>
2025-09-21 00:17:14 +0530

    Add TravelJournal folder as normal files

---
+++ src/TravelJournal/Header.jsx
@@ -0,0 +1,10 @@
+export default function Header(){
+    return(
+        <>
+        <header>
+            <img src="src/TravelJournal/globe1.png" id="logo" alt="globe image"/>
+            <h1 id="header-text">my traveljournal.co</h1>
+        </header>
+        </>
+    )
+}
---
+++ src/TravelJournal/Travel.jsx
@@ -0,0 +1,12 @@
+import Header from "./Header";
+import "./travel.css"
+import Entry from "./Entry";
+export default function Travel(){
+    return(
+        <>
+         <Header/>
+         <Entry/>    
+        </>
+       
+    )
+}
